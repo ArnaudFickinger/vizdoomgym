de001b4158d49d9eb1ae516346f05ad28b163961
Arnaud Fickinger <arnaudfickinger@fb.com>
2021-02-24 13:45:49 -0800

    render

--- vizdoomgym/envs/__init__.py
+++ vizdoomgym/envs/__init__.py
@@ -1,8 +1,12 @@
 from vizdoomgym.envs.vizdoomenv import VizdoomEnv
 from vizdoomgym.envs.vizdoom_env_definitions import (
     VizdoomBasic,
-    VizdoomCorridor,
-    VizdoomCorridorSparse,
+    VizdoomCorridor5,
+VizdoomCorridor1,
+VizdoomCorridor3,
+VizdoomCorridor7,
+    VizdoomCorridorSparse5,
+    VizdoomCorridorSparse1,
     VizdoomDefendCenter,
     VizdoomDefendLine,
     VizdoomHealthGathering,
